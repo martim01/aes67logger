e19dbe27710fd81ee0aeeede0a3e882bcf749756
Matthew Martin <martim01@outlook.com>
2023-02-14 12:04:54 +0000

    fix: work to get encoder script to work

--- encoder/encoder.py
+++ encoder/encoder.py
@@ -12,43 +12,51 @@ import logging
 
 
 def enumerateDir(dir_path, ext):
+    logging.warning("enumerateDir %s %s", dir_path, ext)
     res = []
     try:
         for path in os.listdir(dir_path):
             fullPath = os.path.join(dir_path, path)
             if os.path.isfile(fullPath):
-                split = os.path.splitext(path)[1]
-                if split[1] == ext:
+                split = os.path.splitext(path)
+                if split[1] == "."+ext:
                     res.append(split[0])
     except Exception as e:
-        print(str(e))
+        logging.error(str(e))
     return res
 
 def opusEncode(wavFile, opusPath):
-    logging.info("Encode %s",wavFile)
+    logging.warning("Encode %s",wavFile)
     fileName = os.path.splitext(os.path.basename(wavFile))[0]
     opusFile = os.path.join(opusPath, fileName)
-    subprocess.run(["opusenc", wavFile, opusFile])
-    logging.info("Encoded %s", opusFile)
+    subprocess.run(["opusenc", wavFile, opusFile, '--quiet'])
+    logging.warning("Encoded %s", opusFile)
 
 class Logger():
     def __init__(self, name, wavPath, opusPath):
-        logging.info('Logger %s created', name)
+        logging.warning('Logger %s created', name)
         self.name = name
         self.wavPath = wavPath+"/"+name
         self.opusPath = opusPath+"/"+name
         self.encodeList = []
+        try:
+            os.makedirs(self.opusPath)
+        except FileExistsError:
+            logging.warning('Dir already exists')
 
     def createEncodeList(self):
-        wavList = enumerateDir(self.wavPath, "wav", True)
-        opusSet = set(enumerateDir(self.opusPath, "opus", True))
+        logging.warning('Logger %s workout wav files that still need encoding', self.name)
+        wavList = enumerateDir(self.wavPath, "wav")
+        opusSet = set(enumerateDir(self.opusPath, "opus"))
         # look through the opus directory and remove any file already encoded
         self.encodeList = [file for file in wavList if file not in opusSet]
-        logging.info('Logger %s has %d files to still encode', self.name, len(self.encodeList))
+        logging.warning('Logger %s has %d files to still encode', self.name, len(self.encodeList))
 
     def encode(self, executor):
         for wavFile in self.encodeList:
-            executor.submit(opusEncode, wavFile, self.opusPath)
+            wavFile = wavFile + ".wav"
+            executor.submit(opusEncode, os.path.join(self.wavPath, wavFile), self.opusPath)
+        self.encodeList = []
 
 class CreatedHandler(FileSystemEventHandler):
     def __init__(self, executor, opusPath):
@@ -56,9 +64,9 @@ class CreatedHandler(FileSystemEventHandler):
         self.opusPath = opusPath
 
     def on_created(self, event):
-        logging.info("File %s created",event.src_path)
+        logging.warning("File %s created",event.src_path)
     def on_closed(self, event):
-        logging.info("File %s closed",event.src_path)
+        logging.warning("File %s closed",event.src_path)
         #work out the opus path
         dir = os.path.split(event.src_path)[0]
         components = dir.split('/')
@@ -90,8 +98,9 @@ def run():
     loggers = []
     # find all the possible loggers, create the loggers and work out what files need encoding    
     for logger in enumerateDir(config["paths"].get("loggers", "."), "ini"):
-        loggers[logger] = Logger(logger, config["paths"].get("wav", "."), config["paths"].get("opus", "."))
-        loggers[logger].createEncodeList()
+        loggerObj = Logger(logger, config["paths"].get("wav", "."), config["paths"].get("opus", "."))
+        loggerObj.createEncodeList()
+        loggers.append(loggerObj)
 
     #create the thread pool that will launch the encocders
     executor = concurrent.futures.ThreadPoolExecutor(max_workers=len(loggers)+4)
@@ -103,8 +112,8 @@ def run():
         observer.schedule(event_handler, path=config["paths"].get("wav", "."), recursive=True)
         observer.start()
     
-        for logger in loggers:
-            logger.encode(executor)
+        for loggerObj in loggers:
+            loggerObj.encode(executor)
 
         try:
             while True:
@@ -114,7 +123,7 @@ def run():
             observer.stop()
 
         observer.join()
-        logging.info("Encoder exiting")
+        logging.warning("Encoder exiting")
     except FileNotFoundError:
         logging.critical("Path %s does not exist", config["paths"].get("wav", "."))
         quit();
