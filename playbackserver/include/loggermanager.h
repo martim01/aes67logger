0ccce6abbaa64f3d90498243e30cd327d140e593
Matthew Martin <martim01@outlook>
2023-03-02 07:00:02 +0000

    fix: make watchloggerpath public

--- playbackserver/include/loggermanager.h
+++ playbackserver/include/loggermanager.h
@@ -13,11 +13,11 @@ class LoggerManager
     public:
         LoggerManager(PlaybackServer& server);
         void EnumLoggers();
-
+        void WatchLoggerPath();
         const std::map<std::string, std::shared_ptr<LoggerObserver>>& GetLoggers() const { return m_mLoggers;}
     
     private:
-        void WatchLoggerPath();
+        
         std::shared_ptr<LoggerObserver> CreateLoggerObserver(const std::filesystem::path& iniFile);
         void OnLoggerCreated(int nWd, const std::filesystem::path& path, uint32_t mask, bool bDirectory);
         void OnLoggerDeleted(int nWd, const std::filesystem::path& path, uint32_t mask, bool bDirectory);
